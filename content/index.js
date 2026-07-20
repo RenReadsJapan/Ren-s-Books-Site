5d196c90dc60121372529adf2fdcb3d0be7e5648
RenReadsJapan <onemetal.spider@gmail.com>
2026-07-20 14:27:26 +0900

    Read book 2 online

--- content/index.js
+++ content/index.js
@@ -2,9 +2,11 @@
 // The key MUST match that book's `slug` in data/books.js exactly.
 
 import et01 from './everyday-together/et-01';
+import et02 from './everyday-together/et-02';
 
 const bookContent = {
   'et-01-the-wrong-line-message': et01, // CHECK: does this match the slug in books.js? Fix if not.
+  'et-02-just-say-no-to-overtime': et02,
 };
 
 export default bookContent;
